46fa78200b1a72ef3f543e5c5469d4484f8b4bde
NikhilKansotia <kansotianikhil92@gmail.com>
2024-07-29 10:28:04 +0530

    Connected  to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
 const PORT_NO = 6666;
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to Database successfull");
+  })
+  .catch((error) => {
+    console.log("Error in connecting to database", error);
+  });
+
 app.listen(PORT_NO, () => console.log(`Listening to Port No ${PORT_NO}`));
